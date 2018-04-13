e306bec193f18bbdfbfa3b0e11a39f6a7bd4267d
Angelos Ioannou <t704@FRA-1.local>
2018-04-13 13:20:46 +0200

    Fixed routing

--- track-your-reading/src/App.js
+++ track-your-reading/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar";
 import Sidebar from "./components/sidebar";
@@ -22,12 +23,22 @@ class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider>
-        <div>
-          <Navbar toggleFn={this.toggleNav.bind(this)}/>
-          <Sidebar toggleFn={this.toggleNav.bind(this)} open={this.state.navOpen}/>
-        </div>
-      </MuiThemeProvider>
+      <Router>
+        <MuiThemeProvider>
+          <div>
+            <Navbar toggleFn={this.toggleNav.bind(this)}/>
+            <Sidebar toggleFn={this.toggleNav.bind(this)} open={this.state.navOpen}/>
+            <div>
+              <Route exact path="/"
+                render={()=> <h1>Homepage here</h1>}
+              ></Route>
+              <Route path="/about"
+                render={()=> <h2>And about page will be here</h2>}
+              ></Route>
+            </div>
+          </div>
+        </MuiThemeProvider>
+      </Router>
     );
   }
 }
--- track-your-reading/src/components/sidebar.js
+++ track-your-reading/src/components/sidebar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Drawer from "material-ui/Drawer";
 import MenuItem from "material-ui/MenuItem";
 
@@ -7,22 +7,20 @@ class Sidebar extends React.Component {
 
     render(){
         return(
-            <Router>
-               <Drawer
-                docked={false}
-                open={this.props.open}
-                className="sidebar-nav"
-                onRequestChange={this.props.toggleFn}>
-                    <MenuItem
-                        onClick={this.props.toggleFn}>
-                        <Link to="/">Home</Link>
-                    </MenuItem>
-                    <MenuItem
-                        onClick={this.props.toggleFn}>
-                        <Link to="/about">About</Link>
-                    </MenuItem>
-                </Drawer> 
-            </Router>
+            <Drawer
+            docked={false}
+            open={this.props.open}
+            className="sidebar-nav"
+            onRequestChange={this.props.toggleFn}>
+                <MenuItem
+                    onClick={this.props.toggleFn}>
+                    <Link to="/">Home</Link>
+                </MenuItem>
+                <MenuItem
+                    onClick={this.props.toggleFn}>
+                    <Link to="/about">About</Link>
+                </MenuItem>
+            </Drawer> 
         );
     }
 }
