4dcac31499012bdfe1dca282610fd229d8b0a001
Angelos Ioannou <angelos.ioannou@gmx.de>
2018-04-16 14:49:36 +0200

    added axios and made get request to express api

--- track-your-reading/src/components/home.js
+++ track-your-reading/src/components/home.js
@@ -1,9 +1,37 @@
 import React, {Component} from "react";
+import axios from "axios";
+
+var api = "http://localhost:4000/user";
 
 class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get(api).then((res)=>{
+            this.setState({
+                users: res.data
+            });
+        }).catch((err)=>{
+            console.log("error establishing connection to api");
+        });
+    }
+
     render(){
         return(
-            <h2>Homepage here - {this.props.test}</h2>
+            <div>
+                <h2>Homepage here - {this.props.test}</h2>
+                <ul>
+                    {this.state.users.map((user)=>{
+                        return <li key={user._id}>{user.name}</li>
+                    })}
+                </ul>
+            </div>
         );
     }
 }
